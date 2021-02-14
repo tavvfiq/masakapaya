10ee27940889e19f408cb22af16e23d7a99a8843
tavvfiq <taufiq.w@mail.ugm.ac.id>
2021-02-14 14:45:57 +0700

    feat: add button to left and right

--- src/components/atoms/Header/index.tsx
+++ src/components/atoms/Header/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Colors, Mixins, Typography } from '@styles/index';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 interface Props {
   title: string;
@@ -10,16 +11,18 @@ const styles = StyleSheet.create({
   container: {
     display: 'flex',
     flexDirection: 'row',
-    height: Mixins.scaleSize(60),
+    height: Mixins.heightPercentageToDP('8%'),
     backgroundColor: Colors.WHITE,
+    justifyContent: 'space-between',
     alignItems: 'center',
     borderBottomWidth: 1,
     borderBottomColor: Colors.GRAY_LIGHT,
     ...Mixins.padding(0, 10, 0, 10),
   },
   title: {
-    fontFamily: Typography.FONT_FAMILY_BOLD,
-    fontSize: Mixins.scaleFont(20),
+    fontFamily: Typography.FONT_LOGO,
+    fontSize: Mixins.scaleFont(30),
+    color: Colors.TEXT_COLOR_PRIMARY,
     textAlign: 'center',
     flex: 1,
   },
@@ -28,7 +31,13 @@ const styles = StyleSheet.create({
 const Header = ({ title }: Props) => {
   return (
     <View style={styles.container}>
+      <TouchableOpacity>
+        <Icon name="cog-outline" size={30} color={Colors.SECONDARY} />
+      </TouchableOpacity>
       <Text style={styles.title}>{title}</Text>
+      <TouchableOpacity>
+        <Icon name="list-outline" size={30} color={Colors.SECONDARY} />
+      </TouchableOpacity>
     </View>
   );
 };
