58742588d1cc4d0bb957b2e5a73f9f656c35fadf
tavvfiq <taufiq.w@mail.ugm.ac.id>
2021-02-16 01:12:24 +0700

    feat: leftIcon and rightIcon as props (not default)

--- src/components/atoms/Header/index.tsx
+++ src/components/atoms/Header/index.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Colors, Mixins, Typography } from '@styles/index';
-import Icon from 'react-native-vector-icons/Ionicons';
 
 interface Props {
+  isHome?: boolean;
   title: string;
+  leftIcon?: React.ReactElement;
+  rightIcon?: React.ReactElement;
+  leftIconOnpress?: () => void;
+  rightIconOnPress?: () => void;
 }
+
+export const HEADER_HEIGHT = Mixins.heightPercentageToDP('8%');
 
 const styles = StyleSheet.create({
   container: {
     display: 'flex',
     flexDirection: 'row',
-    height: Mixins.heightPercentageToDP('8%'),
+    height: HEADER_HEIGHT,
     backgroundColor: Colors.WHITE,
     justifyContent: 'space-between',
     alignItems: 'center',
@@ -19,24 +25,36 @@ const styles = StyleSheet.create({
     borderBottomColor: Colors.GRAY_LIGHT,
     ...Mixins.padding(0, 10, 0, 10),
   },
-  title: {
+  titleHome: {
     fontFamily: Typography.FONT_LOGO,
     fontSize: Mixins.scaleFont(30),
     color: Colors.TEXT_COLOR_PRIMARY,
     textAlign: 'center',
     flex: 1,
   },
+  title: {
+    fontFamily: Typography.FONT_FAMILY_MEDIUM,
+    fontSize: Mixins.scaleFont(26),
+    color: Colors.TEXT_COLOR_PRIMARY,
+    textAlign: 'center',
+    flex: 1,
+  },
 });
 
-const Header = ({ title }: Props) => {
+const Header = ({
+  title,
+  leftIcon,
+  rightIcon,
+  leftIconOnpress,
+  rightIconOnPress,
+  isHome,
+}: Props) => {
   return (
     <View style={styles.container}>
-      <TouchableOpacity>
-        <Icon name="cog-outline" size={30} color={Colors.SECONDARY} />
-      </TouchableOpacity>
-      <Text style={styles.title}>{title}</Text>
-      <TouchableOpacity>
-        <Icon name="list-outline" size={30} color={Colors.SECONDARY} />
+      <TouchableOpacity onPress={leftIconOnpress}>{leftIcon}</TouchableOpacity>
+      <Text style={isHome ? styles.titleHome : styles.title}>{title}</Text>
+      <TouchableOpacity onPress={rightIconOnPress}>
+        {rightIcon}
       </TouchableOpacity>
     </View>
   );
