bbc6e7a00e5f4e60ce366216652d38867440e6ab
tavvfiq <taufiq.w@mail.ugm.ac.id>
2021-02-16 01:09:01 +0700

    feat: add recipe detail screen

---
+++ src/screens/RecipeDetail/index.tsx
@@ -0,0 +1,247 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+  Image,
+  ActivityIndicator,
+  NativeSyntheticEvent,
+  NativeScrollEvent,
+} from 'react-native';
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Colors, Mixins, Typography } from '@styles/index';
+import MiniDetail from '@components/atoms/MiniDetail';
+import { ParamList } from '@navigation/RouteTypes';
+import Header, { HEADER_HEIGHT } from '@components/atoms/Header';
+import useRecipeDetail from '@hooks/useRecipeDetail';
+import Animated, {
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated';
+import RoundedButton from '@components/atoms/RoundedButton';
+
+const styles = StyleSheet.create({
+  outerContainer: {
+    backgroundColor: Colors.WHITE,
+  },
+  titleText: {
+    fontFamily: Typography.FONT_FAMILY_MEDIUM,
+    fontSize: Typography.FONT_SIZE_20,
+    color: Colors.TEXT_COLOR_PRIMARY,
+  },
+  contentContainer: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    flexDirection: 'column',
+    height: '100%',
+    backgroundColor: Colors.WHITE,
+    ...Mixins.padding(10, 10, 5, 10),
+  },
+  authorContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  author: {
+    fontFamily: Typography.FONT_FAMILY_MEDIUM,
+    fontSize: Typography.FONT_SIZE_16,
+    color: Colors.TEXT_COLOR_PRIMARY,
+    textAlign: 'center',
+  },
+  publishedDate: {
+    fontFamily: Typography.FONT_FAMILY_REGULAR,
+    fontSize: Typography.FONT_SIZE_16,
+    color: Colors.TEXT_COLOR_SECONDARY,
+    textAlign: 'center',
+  },
+  thumbnail: {
+    height: Mixins.heightPercentageToDP('60%'),
+    width: '100%',
+    resizeMode: 'cover',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: Colors.BORDER_COLOR,
+  },
+  subTitle: {
+    fontFamily: Typography.FONT_FAMILY_MEDIUM,
+    fontSize: Typography.FONT_SIZE_16,
+    color: Colors.TEXT_COLOR_PRIMARY,
+    alignSelf: 'flex-start',
+    ...Mixins.margin(4, 0, 4, 0),
+  },
+  desc: {
+    fontFamily: Typography.FONT_FAMILY_REGULAR,
+    textAlign: 'justify',
+  },
+  box: {
+    width: Mixins.scaleSize(32),
+  },
+  separator: {
+    height: 1,
+    width: '80%',
+    backgroundColor: Colors.BORDER_COLOR,
+    ...Mixins.margin(5, 0, 5, 0),
+  },
+  footer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.WHITE,
+    height: Mixins.heightPercentageToDP('25%'),
+  },
+  errorMessage: {
+    fontFamily: Typography.FONT_FAMILY_MEDIUM,
+  },
+  ingredientsList: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  item: {
+    fontFamily: Typography.FONT_FAMILY_REGULAR,
+    alignSelf: 'flex-start',
+    textAlign: 'left',
+  },
+  refreshButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+const RecipeDetail = () => {
+  const navigation = useNavigation();
+  const route = useRoute<RouteProp<ParamList, 'RecipeDetail'>>();
+  const { content } = route.params;
+  const scrollY = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
+  const [loading, error, recipeDetail, reload] = useRecipeDetail(content.key);
+
+  const goBack = () => {
+    navigation.goBack();
+  };
+
+  const onScrollEvent = ({
+    nativeEvent,
+  }: NativeSyntheticEvent<NativeScrollEvent>) => {
+    const { velocity, contentOffset } = nativeEvent;
+    scrollY.value = contentOffset.y - HEADER_HEIGHT;
+    if (velocity) {
+      if (velocity.y < 0) {
+        translateY.value = interpolate(
+          scrollY.value,
+          [-HEADER_HEIGHT, scrollY.value],
+          [0, scrollY.value],
+          Animated.Extrapolate.CLAMP,
+        );
+      } else if (velocity.y > 0) {
+        translateY.value = interpolate(
+          scrollY.value,
+          [-HEADER_HEIGHT, HEADER_HEIGHT],
+          [0, scrollY.value],
+          Animated.Extrapolate.CLAMP,
+        );
+      }
+    }
+    console.log({
+      scrollY: scrollY.value,
+      translateY: translateY.value,
+      contentOffsetY: contentOffset.y,
+    });
+  };
+
+  const animateHeader = useAnimatedStyle(() => {
+    return {
+      zIndex: 10,
+      width: '100%',
+      // transform: [{ translateY: translateY.value }],
+    };
+  });
+
+  return (
+    <>
+      <ScrollView
+        stickyHeaderIndices={[0]}
+        onScroll={onScrollEvent}
+        scrollEventThrottle={16}
+        contentContainerStyle={styles.outerContainer}
+        showsVerticalScrollIndicator={false}>
+        <Animated.View style={animateHeader}>
+          <Header
+            title="Detail"
+            leftIcon={
+              <Icon name="arrow-left" color={Colors.SECONDARY} size={32} />
+            }
+            leftIconOnpress={goBack}
+            rightIcon={<View style={styles.box} />}
+          />
+        </Animated.View>
+        <View style={styles.contentContainer}>
+          <Image style={styles.thumbnail} source={{ uri: content.thumb }} />
+          <Text style={styles.titleText}>{content.title}</Text>
+          <MiniDetail
+            times={content.times}
+            servings={content.portion}
+            dificulty={content.dificulty}
+          />
+          {recipeDetail ? (
+            <>
+              <View style={styles.authorContainer}>
+                <Text style={styles.author}>
+                  {recipeDetail?.author.user || 'Tidak diketahui'}
+                </Text>
+                <Text style={styles.publishedDate}>
+                  {recipeDetail?.author.datePublished || 'Tidak diketahui'}
+                </Text>
+              </View>
+              <View style={styles.separator} />
+              <Text style={styles.desc}>{recipeDetail?.desc}</Text>
+              <View style={styles.separator} />
+              <Text style={styles.subTitle}>Bahan-bahan</Text>
+              <View style={styles.ingredientsList}>
+                {recipeDetail.ingredient?.map((item, index) => {
+                  return (
+                    <Text style={styles.item} key={index}>
+                      {item}
+                    </Text>
+                  );
+                })}
+              </View>
+              <Text style={styles.subTitle}>Cara membuat</Text>
+              <View style={styles.ingredientsList}>
+                {recipeDetail.step?.map((item, index) => {
+                  return (
+                    <Text style={styles.item} key={index}>
+                      {item}
+                    </Text>
+                  );
+                })}
+              </View>
+            </>
+          ) : (
+            <View style={styles.footer}>
+              {loading && !error ? (
+                <ActivityIndicator size="small" color={Colors.GRAY_MEDIUM} />
+              ) : (
+                <>
+                  <Text style={styles.errorMessage}>Server error</Text>
+                  <RoundedButton
+                    onPress={reload}
+                    radius={32}
+                    containerStyle={styles.refreshButton}>
+                    <Icon name="refresh" size={32} color={Colors.SECONDARY} />
+                  </RoundedButton>
+                </>
+              )}
+            </View>
+          )}
+        </View>
+      </ScrollView>
+    </>
+  );
+};
+
+export default RecipeDetail;
