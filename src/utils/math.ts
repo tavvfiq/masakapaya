e7e30d6d4ba8759b57a8c18591d270ec2ac6586f
tavvfiq <taufiq.w@mail.ugm.ac.id>
2021-02-16 17:28:07 +0700

    feat: add function filter array on utils

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,3 +1,11 @@
 export const generateRandomPage = () => {
   return Math.round(Math.random() * 149 + 1);
 };
+
+export function filterArray<T>(
+  arr: T[],
+  callback: (value: T, index: number, array: T[]) => boolean,
+): T[] {
+  const filtered = arr.filter(callback);
+  return filtered;
+}
