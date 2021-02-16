d41c9d17cc080248e715f656cdf82a29db79ed31
tavvfiq <taufiq.w@mail.ugm.ac.id>
2021-02-16 17:29:58 +0700

    style: add font size variation

--- src/styles/typography.ts
+++ src/styles/typography.ts
@@ -11,6 +11,9 @@ export const FONT_WEIGHT_REGULAR = '400';
 export const FONT_WEIGHT_BOLD = '700';
 
 // FONT SIZE
+export const FONT_SIZE_30 = scaleFont(30);
+export const FONT_SIZE_28 = scaleFont(28);
+export const FONT_SIZE_26 = scaleFont(26);
 export const FONT_SIZE_24 = scaleFont(24);
 export const FONT_SIZE_20 = scaleFont(20);
 export const FONT_SIZE_18 = scaleFont(18);
